96795dfd54f97cbafca291960489aee3164b8188
NataniaDamons <104576160+NataniaDamons@users.noreply.github.com>
2024-05-19 13:38:36 +0200

    the class and method to handle searches on Maps

---
+++ src/PlaywrightFrameworkTest/Automation.Common/Pages/GoogleMapsPage.cs
@@ -0,0 +1,17 @@
+using Microsoft.Playwright;
+using PlaywrightFrameworkTest.Automation.Common.Utilities;
+
+namespace PlaywrightFrameworkTest.Automation.Common.Pages
+{
+    public class GoogleMapsPage : Automation
+    {
+        public async Task SearchAddress(IPage page, string address)
+       {
+           await Assertions.Expect(page.GetByRole(AriaRole.Link, new() { Name = "Sign in" })).ToBeVisibleAsync(new() { Timeout = 1000 * 10 });
+           await page.GetByLabel("Search Google Maps").FillAsync(address);
+           await page.GetByRole(AriaRole.Gridcell, new() { Name = address }).ClickAsync();
+       }
+
+    }
+
+}
