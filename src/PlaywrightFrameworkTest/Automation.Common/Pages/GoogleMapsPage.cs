199d06c2823d9106a8910285c1744fec1d759339
NataniaDamons <104576160+NataniaDamons@users.noreply.github.com>
2024-05-19 20:50:14 +0200

    Validate that the correct address is loaded

--- src/PlaywrightFrameworkTest/Automation.Common/Pages/GoogleMapsPage.cs
+++ src/PlaywrightFrameworkTest/Automation.Common/Pages/GoogleMapsPage.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.Playwright;
 using PlaywrightFrameworkTest.Automation.Common.Utilities;
 
@@ -6,12 +7,19 @@ namespace PlaywrightFrameworkTest.Automation.Common.Pages
     public class GoogleMapsPage : Automation
     {
         public async Task SearchAddress(IPage page, string address)
-       {
+        {
            await Assertions.Expect(page.GetByRole(AriaRole.Link, new() { Name = "Sign in" })).ToBeVisibleAsync(new() { Timeout = 1000 * 20 });
            await page.GetByLabel("Search Google Maps").FillAsync(address);
            await page.GetByRole(AriaRole.Gridcell, new() { Name = address }).ClickAsync();
-       }
+        }
 
-    }
+        public async Task<bool> ValidateAddress(IPage page, string address)
+        {
+            await Assertions.Expect(page.Locator(".DUwDvf.lfPIob")).ToBeVisibleAsync(new() { Timeout = 1000 * 20 });
+            var searchBoxValue = await page.TextContentAsync(".DUwDvf.lfPIob");
+            return searchBoxValue != null && address.Contains(searchBoxValue);
+        }
 
+
+   }
 }
--- src/PlaywrightFrameworkTest/Automation.Common/Utilities/Dataprovider.cs
+++ src/PlaywrightFrameworkTest/Automation.Common/Utilities/Dataprovider.cs
@@ -7,7 +7,7 @@ namespace PlaywrightFrameworkTest.Automation.Common.Utilities
         public static IEnumerable<object[]>SearchAddresses()
         {
             yield return new object[] {"Menlyn Park Shopping Centre Lois Avenue, Menlyn, Pretoria"};
-            yield return new object[] {"1241 Embankment Rd, Zwartkop, Centurion, 0051"};
+            yield return new object[] {"Voortrekker Monument Monument Street, Groenkloof 358-Jr, Pretoria"};
             yield return new object[] {"Christina De Wit Ave Groenkloof 358-Jr, Pretoria, 0027"};
             yield return new object[] {"Pretoria Art Museum Wessels Street, Arcadia, Pretoria"}; 
         }
