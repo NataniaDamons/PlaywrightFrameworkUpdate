700fc5c5869953bf47b30c2d3b788296cb91ccba
NataniaDamons <104576160+NataniaDamons@users.noreply.github.com>
2024-05-19 13:40:46 +0200

    Deleted unnecessary parameters

--- src/PlaywrightFrameworkTest/Automation.Common/Configuration/TestSettings.cs
+++ src/PlaywrightFrameworkTest/Automation.Common/Configuration/TestSettings.cs
@@ -15,14 +15,8 @@ public record TestSettings
     //Gets and sets operation speed 
     public int OperationSpeed { get; set; }
 
-    //get the settings for the accounts available 
-    public Account[]? Accounts { get; set; }
-
     //setup Debugging options 
     public Debugging? Debugging { get; set; }
-
-    //Determine how many parallel login attempts should be executed at the same time
-    public int LoginParallelism { get; set; }
 
     //Manage viewports
     public Viewport? Viewport  { get; set;}
